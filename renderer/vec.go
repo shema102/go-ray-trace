601b89b0eb8efa91e04b7f8fd3369999ef3bd43e
shema102 <sergeishemshur@gmail.com>
2023-03-23 21:15:20 +0200

    feat: add more vector operations

--- renderer/vec.go
+++ renderer/vec.go
@@ -28,6 +28,30 @@ func (v Vec3) DivScalar(s float64) Vec3 {
 	return Vec3{v.X / s, v.Y / s, v.Z / s}
 }
 
+func (v Vec3) Dot(v2 Vec3) float64 {
+	return v.X*v2.X + v.Y*v2.Y + v.Z*v2.Z
+}
+
+func (v Vec3) Cross(v2 Vec3) Vec3 {
+	return Vec3{
+		v.Y*v2.Z - v.Z*v2.Y,
+		v.Z*v2.X - v.X*v2.Z,
+		v.X*v2.Y - v.Y*v2.X,
+	}
+}
+
+func (v Vec3) Negate() Vec3 {
+	return Vec3{-v.X, -v.Y, -v.Z}
+}
+
+func (v Vec3) UnitVector() Vec3 {
+	return v.DivScalar(v.Length())
+}
+
+func (v Vec3) Reflect(n Vec3) Vec3 {
+	return v.Sub(n.MulScalar(2 * v.Dot(n)))
+}
+
 func (v Vec3) LengthSquared() float64 {
 	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
 }
